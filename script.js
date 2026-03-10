1a2e559576da4d5d742a19573f9e327ffb1ce9b1
saga <g-07124378@moe-dl.edu.my>
2026-03-10 14:33:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,104 @@
+// ===============================
+// SISTEM TEMPAHAN BAS SAGA
+// SM SAINS ALOR GAJAH
+// ===============================
+
+
+// Papar ruangan jika pilih kenderaan lain
 function checkKenderaan(){
 
-var k=document.getElementById("kenderaan").value
+var kenderaan = document.getElementById("kenderaan").value
 
-if(k=="Lain-lain"){
-document.getElementById("lain").style.display="block"
-}
-else{
-document.getElementById("lain").style.display="none"
-}
+if(kenderaan === "Lain-lain"){
+
+document.getElementById("lain").style.display = "block"
+
+}else{
+
+document.getElementById("lain").style.display = "none"
 
 }
 
+}
+
+
+
+// Hantar tempahan ke Google Sheets
 function hantar(){
 
-var pemandu=[]
+// ambil pemandu
+var pemandu = []
 
-if(document.getElementById("faizal").checked)
+if(document.getElementById("faizal").checked){
 pemandu.push("En. Faizal")
+}
 
-if(document.getElementById("nuri").checked)
+if(document.getElementById("nuri").checked){
 pemandu.push("En. Nuri")
+}
 
-var data={
 
-tarikhMohon:document.getElementById("tarikhMohon").value,
-aktiviti:document.getElementById("aktiviti").value,
-tempat:document.getElementById("tempat").value,
-nama:document.getElementById("nama").value,
-jawatan:document.getElementById("jawatan").value,
-lelaki:document.getElementById("lelaki").value,
-perempuan:document.getElementById("perempuan").value,
-guru:document.getElementById("guru").value,
-tarikhPergi:document.getElementById("tarikhPergi").value,
-masaPergi:document.getElementById("masaPergi").value,
-masaBalik:document.getElementById("masaBalik").value,
-kenderaan:document.getElementById("kenderaan").value,
-kenderaanLain:document.getElementById("kenderaanLain").value,
-pemandu:pemandu.join(", ")
+
+// bina data tempahan
+var data = {
+
+tarikhMohon : document.getElementById("tarikhMohon").value,
+aktiviti : document.getElementById("aktiviti").value,
+tempat : document.getElementById("tempat").value,
+nama : document.getElementById("nama").value,
+jawatan : document.getElementById("jawatan").value,
+
+lelaki : document.getElementById("lelaki").value,
+perempuan : document.getElementById("perempuan").value,
+guru : document.getElementById("guru").value,
+
+tarikhPergi : document.getElementById("tarikhPergi").value,
+masaPergi : document.getElementById("masaPergi").value,
+masaBalik : document.getElementById("masaBalik").value,
+
+kenderaan : document.getElementById("kenderaan").value,
+kenderaanLain : document.getElementById("kenderaanLain").value,
+
+pemandu : pemandu.join(", ")
 
 }
 
-fetch("PASTE_APPS_SCRIPT_URL",{
 
-method:"POST",
 
-body:JSON.stringify(data)
+// URL GOOGLE APPS SCRIPT
+var url = "https://script.google.com/macros/s/AKfycbxadg3jzotgg2cyTAoO8sJnlbWZs0LfNAbsNWnevVMMCEaA8uvKwmc7kpwOwuqBHlcwxQ/exec"
+
+
+
+// hantar ke Google Sheet
+fetch(url, {
+
+method: "POST",
+
+body: JSON.stringify(data),
+
+headers:{
+"Content-Type":"application/json"
+}
 
 })
 
-.then(res=>res.json())
+.then(response => response.json())
 
-.then(res=>{
+.then(result => {
 
-alert("Tempahan berjaya dihantar")
+alert("Tempahan bas berjaya dihantar!")
+
+// reset borang selepas hantar
+document.querySelectorAll("input").forEach(input => input.value="")
+
+})
+
+.catch(error => {
+
+alert("Ralat menghantar tempahan")
+
+console.log(error)
 
 })
 
