32918fd69773a0da3839cc37fc9579a917452c88
saga <g-07124378@moe-dl.edu.my>
2026-03-11 11:45:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function hantar(){
+function hantarTempahan(){
 
+let kenderaan=[];
 let pemandu=[];
+
+
+if(document.getElementById("bas").checked){
+kenderaan.push("Bas");
+}
+
+if(document.getElementById("coaster").checked){
+kenderaan.push("Coaster");
+}
+
+if(document.getElementById("van").checked){
+kenderaan.push("Van");
+}
+
 
 if(document.getElementById("faizal").checked){
 pemandu.push("En. Faizal");
@@ -10,11 +25,22 @@ if(document.getElementById("nuri").checked){
 pemandu.push("En. Nuri");
 }
 
+
+if(kenderaan.length==0){
+
+alert("Sila pilih kenderaan");
+
+return;
+
+}
+
+
 let data={
 
 tarikhMohon:document.getElementById("tarikhMohon").value,
 aktiviti:document.getElementById("aktiviti").value,
 tempat:document.getElementById("tempat").value,
+
 nama:document.getElementById("nama").value,
 jawatan:document.getElementById("jawatan").value,
 
@@ -28,17 +54,20 @@ tarikhPulang:document.getElementById("tarikhPulang").value,
 masaPergi:document.getElementById("masaPergi").value,
 masaBalik:document.getElementById("masaBalik").value,
 
-kenderaan:document.getElementById("kenderaan").value,
+kenderaan:kenderaan.join(", "),
 pemandu:pemandu.join(", ")
 
 };
 
-fetch("https://script.google.com/macros/s/AKfycbxLh29v0OuPxCOW-7KX5WP36QNm0eDJw4THBQExEvI-kb_5NdnzkkztjfiO3cqadD3MGA/exec",{
+
+fetch("https://script.google.com/macros/s/AKfycbxaEm7o8hIe--aTlcw44V7sn_ulfQuOm8Y03z04veoqX__XHzrkWsHHd18S53hSjzOi/exec",{
 
 method:"POST",
+mode:"no-cors",
 body:JSON.stringify(data)
 
 })
+
 .then(()=>{
 
 alert("Tempahan berjaya dihantar");
@@ -46,14 +75,11 @@ alert("Tempahan berjaya dihantar");
 document.getElementById("borangTempahan").reset();
 
 setTimeout(function(){
-window.location.href="dashboard.html";
-},1500);
 
-})
-.catch(err=>{
-alert("Tempahan gagal");
-console.log(err);
+window.location.href="dashboard.html";
+
+},1000);
+
 });
 
 }
-
